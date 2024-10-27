1c9d31bd596b0591aa097c6e2d8e6ab74a011271
AbhishekWadhai <abhiabhi9488@gmail.com>
2024-10-27 12:13:10 +0530

    new permissions

--- lib/model/uauc_model.dart
+++ lib/model/uauc_model.dart
@@ -1,79 +1,72 @@
 import 'dart:convert';
 
 class UaUc {
-  String id;
-  ProjectName projectName;
-  String area;
-  String date;
-  String time;
-  ActionTakenBy? observer;
-  String observation;
-  String photo;
-  List<Hazard> hazards;
-  List<String> causes;
-  RiskValue riskValue;
-  ActionTakenBy? assignedTo;
-  String correctivePreventiveAction;
-  ActionTakenBy? actionTakenBy;
-  String geotagging;
-  String? comment;
-  int v;
-  String status;
+    String id;
+    ProjectName projectName;
+    String area;
+    String date;
+    String time;
+    ActionTakenBy? observer;
+    String observation;
+    String? photo;
+    List<Hazard> hazards;
+    List<String> causes;
+    RiskValue riskValue;
+    ActionTakenBy? assignedTo;
+    String correctivePreventiveAction;
+    ActionTakenBy? actionTakenBy;
+    String geotagging;
+    String? comment;
+    int v;
+    String status;
 
-  UaUc({
-    required this.id,
-    required this.projectName,
-    required this.area,
-    required this.date,
-    required this.time,
-    required this.observer,
-    required this.observation,
-    required this.photo,
-    required this.hazards,
-    required this.causes,
-    required this.riskValue,
-    required this.assignedTo,
-    required this.correctivePreventiveAction,
-    required this.actionTakenBy,
-    required this.geotagging,
-    this.comment,
-    required this.v,
-    required this.status,
-  });
+    UaUc({
+        required this.id,
+        required this.projectName,
+        required this.area,
+        required this.date,
+        required this.time,
+        required this.observer,
+        required this.observation,
+        this.photo,
+        required this.hazards,
+        required this.causes,
+        required this.riskValue,
+        required this.assignedTo,
+        required this.correctivePreventiveAction,
+        required this.actionTakenBy,
+        required this.geotagging,
+        this.comment,
+        required this.v,
+        required this.status,
+    });
 
-  factory UaUc.fromRawJson(String str) => UaUc.fromJson(json.decode(str));
+    factory UaUc.fromRawJson(String str) => UaUc.fromJson(json.decode(str));
 
-  String toRawJson() => json.encode(toJson());
+    String toRawJson() => json.encode(toJson());
 
-  factory UaUc.fromJson(Map<String, dynamic> json) => UaUc(
+    factory UaUc.fromJson(Map<String, dynamic> json) => UaUc(
         id: json["_id"],
         projectName: ProjectName.fromJson(json["projectName"]),
         area: json["area"],
         date: json["date"],
         time: json["time"],
-        observer: json["observer"] == null
-            ? null
-            : ActionTakenBy.fromJson(json["observer"]),
+        observer: json["observer"] == null ? null : ActionTakenBy.fromJson(json["observer"]),
         observation: json["observation"],
         photo: json["photo"],
-        hazards:
-            List<Hazard>.from(json["hazards"].map((x) => Hazard.fromJson(x))),
+        hazards: List<Hazard>.from(json["hazards"].map((x) => Hazard.fromJson(x))),
         causes: List<String>.from(json["causes"].map((x) => x)),
         riskValue: RiskValue.fromJson(json["riskValue"]),
-        assignedTo: json["assignedTo"] == null
-            ? null
-            : ActionTakenBy.fromJson(json["assignedTo"]),
+        assignedTo: json["assignedTo"] == null ? null : ActionTakenBy.fromJson(json["assignedTo"]),
         correctivePreventiveAction: json["correctivePreventiveAction"],
-        actionTakenBy: json["actionTakenBy"] == null
-            ? null
-            : ActionTakenBy.fromJson(json["actionTakenBy"]),
+        actionTakenBy: json["actionTakenBy"] == null ? null : ActionTakenBy.fromJson(json["actionTakenBy"]),
         geotagging: json["geotagging"],
         comment: json["comment"],
         v: json["__v"],
         status: json["status"],
-      );
+    );
 
-  Map<String, dynamic> toJson() => {
+    Map<String, dynamic> toJson() => {
         "_id": id,
         "projectName": projectName.toJson(),
         "area": area,
@@ -92,48 +85,47 @@ class UaUc {
         "comment": comment,
         "__v": v,
         "status": status,
-      };
+    };
 }
 
 class ActionTakenBy {
-  String id;
-  String userId;
-  String name;
-  String photo;
-  String role;
-  String emailId;
-  String password;
-  String phone;
-  String address;
-  bool isActive;
-  List<String> projectName;
-  DateTime createdAt;
-  DateTime updatedAt;
-  int v;
+    String id;
+    String userId;
+    String name;
+    String photo;
+    String role;
+    String emailId;
+    String password;
+    String phone;
+    String address;
+    bool isActive;
+    List<String> projectName;
+    DateTime createdAt;
+    DateTime updatedAt;
+    int v;
 
-  ActionTakenBy({
-    required this.id,
-    required this.userId,
-    required this.name,
-    required this.photo,
-    required this.role,
-    required this.emailId,
-    required this.password,
-    required this.phone,
-    required this.address,
-    required this.isActive,
-    required this.projectName,
-    required this.createdAt,
-    required this.updatedAt,
-    required this.v,
-  });
+    ActionTakenBy({
+        required this.id,
+        required this.userId,
+        required this.name,
+        required this.photo,
+        required this.role,
+        required this.emailId,
+        required this.password,
+        required this.phone,
+        required this.address,
+        required this.isActive,
+        required this.projectName,
+        required this.createdAt,
+        required this.updatedAt,
+        required this.v,
+    });
 
-  factory ActionTakenBy.fromRawJson(String str) =>
-      ActionTakenBy.fromJson(json.decode(str));
+    factory ActionTakenBy.fromRawJson(String str) => ActionTakenBy.fromJson(json.decode(str));
 
-  String toRawJson() => json.encode(toJson());
+    String toRawJson() => json.encode(toJson());
 
-  factory ActionTakenBy.fromJson(Map<String, dynamic> json) => ActionTakenBy(
+    factory ActionTakenBy.fromJson(Map<String, dynamic> json) => ActionTakenBy(
         id: json["_id"],
         userId: json["userId"],
         name: json["name"],
@@ -148,9 +140,9 @@ class ActionTakenBy {
         createdAt: DateTime.parse(json["createdAt"]),
         updatedAt: DateTime.parse(json["updatedAt"]),
         v: json["__v"],
-      );
+    );
 
-  Map<String, dynamic> toJson() => {
+    Map<String, dynamic> toJson() => {
         "_id": id,
         "userId": userId,
         "name": name,
@@ -165,88 +157,67 @@ class ActionTakenBy {
         "createdAt": createdAt.toIso8601String(),
         "updatedAt": updatedAt.toIso8601String(),
         "__v": v,
-      };
+    };
 }
 
 class Hazard {
-  Id id;
-  Hazards hazards;
-  int v;
+    String id;
+    String hazards;
+    int v;
 
-  Hazard({
-    required this.id,
-    required this.hazards,
-    required this.v,
-  });
+    Hazard({
+        required this.id,
+        required this.hazards,
+        required this.v,
+    });
 
-  factory Hazard.fromRawJson(String str) => Hazard.fromJson(json.decode(str));
+    factory Hazard.fromRawJson(String str) => Hazard.fromJson(json.decode(str));
 
-  String toRawJson() => json.encode(toJson());
+    String toRawJson() => json.encode(toJson());
 
-  factory Hazard.fromJson(Map<String, dynamic> json) => Hazard(
-        id: idValues.map[json["_id"]]!,
-        hazards: hazardsValues.map[json["hazards"]]!,
+    factory Hazard.fromJson(Map<String, dynamic> json) => Hazard(
+        id: json["_id"],
+        hazards: json["hazards"],
         v: json["__v"],
-      );
+    );
 
-  Map<String, dynamic> toJson() => {
-        "_id": idValues.reverse[id],
-        "hazards": hazardsValues.reverse[hazards],
+    Map<String, dynamic> toJson() => {
+        "_id": id,
+        "hazards": hazards,
         "__v": v,
-      };
+    };
 }
-
-enum Hazards { ELECTRICAL_SHOCK_RISK, FIRE_HAZARD, SLIP_AND_FALL }
-
-final hazardsValues = EnumValues({
-  "Electrical Shock Risk": Hazards.ELECTRICAL_SHOCK_RISK,
-  "Fire Hazard": Hazards.FIRE_HAZARD,
-  "Slip and Fall": Hazards.SLIP_AND_FALL
-});
-
-enum Id {
-  THE_66_BB4_DF6_DA47_C4_A04_AD62_C12,
-  THE_66_BB4_DFBDA47_C4_A04_AD62_C13,
-  THE_66_BB4_DFFDA47_C4_A04_AD62_C14
-}
-
-final idValues = EnumValues({
-  "66bb4df6da47c4a04ad62c12": Id.THE_66_BB4_DF6_DA47_C4_A04_AD62_C12,
-  "66bb4dfbda47c4a04ad62c13": Id.THE_66_BB4_DFBDA47_C4_A04_AD62_C13,
-  "66bb4dffda47c4a04ad62c14": Id.THE_66_BB4_DFFDA47_C4_A04_AD62_C14
-});
 
 class ProjectName {
-  String id;
-  String projectId;
-  String projectName;
-  String siteLocation;
-  DateTime startDate;
-  DateTime endDate;
-  String status;
-  String description;
-  String company;
-  int v;
+    String id;
+    String projectId;
+    String projectName;
+    String siteLocation;
+    DateTime startDate;
+    DateTime endDate;
+    String status;
+    String description;
+    String company;
+    int v;
 
-  ProjectName({
-    required this.id,
-    required this.projectId,
-    required this.projectName,
-    required this.siteLocation,
-    required this.startDate,
-    required this.endDate,
-    required this.status,
-    required this.description,
-    required this.company,
-    required this.v,
-  });
+    ProjectName({
+        required this.id,
+        required this.projectId,
+        required this.projectName,
+        required this.siteLocation,
+        required this.startDate,
+        required this.endDate,
+        required this.status,
+        required this.description,
+        required this.company,
+        required this.v,
+    });
 
-  factory ProjectName.fromRawJson(String str) =>
-      ProjectName.fromJson(json.decode(str));
+    factory ProjectName.fromRawJson(String str) => ProjectName.fromJson(json.decode(str));
 
-  String toRawJson() => json.encode(toJson());
+    String toRawJson() => json.encode(toJson());
 
-  factory ProjectName.fromJson(Map<String, dynamic> json) => ProjectName(
+    factory ProjectName.fromJson(Map<String, dynamic> json) => ProjectName(
         id: json["_id"],
         projectId: json["projectId"],
         projectName: json["projectName"],
@@ -257,9 +228,9 @@ class ProjectName {
         description: json["description"],
         company: json["company"],
         v: json["__v"],
-      );
+    );
 
-  Map<String, dynamic> toJson() => {
+    Map<String, dynamic> toJson() => {
         "_id": id,
         "projectId": projectId,
         "projectName": projectName,
@@ -270,66 +241,49 @@ class ProjectName {
         "description": description,
         "company": company,
         "__v": v,
-      };
+    };
 }
 
 class RiskValue {
-  String id;
-  int value;
-  String severity;
-  String alertTimeline;
-  String repeatWarning;
-  int v;
-  List<String>? escalationAlert;
+    String id;
+    int value;
+    String severity;
+    String alertTimeline;
+    String repeatWarning;
+    int v;
+    List<String>? escalationAlert;
 
-  RiskValue({
-    required this.id,
-    required this.value,
-    required this.severity,
-    required this.alertTimeline,
-    required this.repeatWarning,
-    required this.v,
-    this.escalationAlert,
-  });
+    RiskValue({
+        required this.id,
+        required this.value,
+        required this.severity,
+        required this.alertTimeline,
+        required this.repeatWarning,
+        required this.v,
+        this.escalationAlert,
+    });
 
-  factory RiskValue.fromRawJson(String str) =>
-      RiskValue.fromJson(json.decode(str));
+    factory RiskValue.fromRawJson(String str) => RiskValue.fromJson(json.decode(str));
 
-  String toRawJson() => json.encode(toJson());
+    String toRawJson() => json.encode(toJson());
 
-  factory RiskValue.fromJson(Map<String, dynamic> json) => RiskValue(
+    factory RiskValue.fromJson(Map<String, dynamic> json) => RiskValue(
         id: json["_id"],
         value: json["value"],
         severity: json["severity"],
         alertTimeline: json["alertTimeline"],
         repeatWarning: json["repeatWarning"],
         v: json["__v"],
-        escalationAlert: json["escalationAlert"] == null
-            ? []
-            : List<String>.from(json["escalationAlert"]!.map((x) => x)),
-      );
+        escalationAlert: json["escalationAlert"] == null ? [] : List<String>.from(json["escalationAlert"]!.map((x) => x)),
+    );
 
-  Map<String, dynamic> toJson() => {
+    Map<String, dynamic> toJson() => {
         "_id": id,
         "value": value,
         "severity": severity,
         "alertTimeline": alertTimeline,
         "repeatWarning": repeatWarning,
         "__v": v,
-        "escalationAlert": escalationAlert == null
-            ? []
-            : List<dynamic>.from(escalationAlert!.map((x) => x)),
-      };
-}
-
-class EnumValues<T> {
-  Map<String, T> map;
-  late Map<T, String> reverseMap;
-
-  EnumValues(this.map);
-
-  Map<T, String> get reverse {
-    reverseMap = map.map((k, v) => MapEntry(v, k));
-    return reverseMap;
-  }
+        "escalationAlert": escalationAlert == null ? [] : List<dynamic>.from(escalationAlert!.map((x) => x)),
+    };
 }
